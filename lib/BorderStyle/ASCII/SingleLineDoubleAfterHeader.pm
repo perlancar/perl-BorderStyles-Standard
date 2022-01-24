0eb349628ca415b6b386336244e32e79ed3740bb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-01-24 19:19:33 +0700

    Revert previous commit of making {ASCII,UTF8}/SingleLineDoubleAfterHeader dynamic, because per-char args passing is complex for Text::Table::More

--- lib/BorderStyle/ASCII/SingleLineDoubleAfterHeader.pm
+++ lib/BorderStyle/ASCII/SingleLineDoubleAfterHeader.pm
@@ -12,7 +12,6 @@ use parent 'BorderStyleBase';
 our %BORDER = (
     v => 2,
     summary => 'Just like ASCII::SingleLine but uses double line to separate header row and first data row',
-    dynamic => 1,
     chars => [
         ['.','-','+','.'], # 0
         ['|','|','|'],     # 1
@@ -24,38 +23,9 @@ our %BORDER = (
         [], # 6
         [], # 7
 
-        ['+','=','+','+', '+','+','+','+'], # 8 - separator between header rows
+        ['+','-','+','+', '+','+','+','+'], # 8 - separator between header rows
     ],
-    args => {
-        header_row_count => {
-            summary => 'Number of header rows',
-            schema => 'uint*',
-            default => 1,
-            description => <<'_',
-
-This argument is useful if you have multiple header rows.
-
-_
-        },
-    },
 );
-
-sub get_border_char {
-    my ($self, $y, $x, $n, $args) = @_;
-    $args = {} unless defined $args;
-
-    my $in_header = defined $args->{rownum} ?
-        $args->{rownum} < $self->{args}{header_row_count} :
-        ($y >= 0 && $y <= 2 || $y == 8);
-    #use DD; dd $self->{args}; dd $args; dd {in_header=>$in_header, y=>$y, x=>$x, n=>$n};
-    if ($in_header) {
-        if ($y == 3) { $y = 1 }
-        elsif ($y == 4) { $y = 8 }
-    }
-    #use DD; dd {y=>$y, x=>$x, n=>$n};
-
-    $self->SUPER::get_border_char($y, $x, $n, $args);
-}
 
 1;
 # ABSTRACT:
